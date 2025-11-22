369b20161d5c3136fb2b78b943c9c3523b5c71e7
pyw0w <myxi2002@gmail.com>
2025-11-23 02:58:20 +0500

    Add command handling for temporary voice channel creation in VoiceTemp plugin
    - Introduced `commands` method to define a new command "create_temp".
    - Implemented event handling for command interactions to create a temporary voice channel.
    - Added response handling for successful and failed channel creation.
    - Enhanced logging for voice state updates.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use aniapi::{export_plugin, Context, Event, Plugin, Result};
+use aniapi::{export_plugin, CommandSpec, Context, Event, Plugin, Result};
 use async_trait::async_trait;
 use tracing::info;
 
@@ -11,6 +11,12 @@ impl Plugin for VoiceTempPlugin {
         "VoiceTempPlugin"
     }
 
+    fn commands(&self) -> Vec<CommandSpec> {
+        vec![
+            CommandSpec::new("create_temp", "Create a temporary voice channel"),
+        ]
+    }
+
     async fn on_load(&mut self, _ctx: &Context) -> Result<()> {
         info!("VoiceTempPlugin loaded!");
         Ok(())
@@ -21,8 +27,54 @@ impl Plugin for VoiceTempPlugin {
         Ok(())
     }
 
-    async fn on_event(&mut self, event: &Event, _ctx: &Context) -> Result<()> {
+    async fn on_event(&mut self, event: &Event, ctx: &Context) -> Result<()> {
         match event {
+            Event::Interaction(interaction) => {
+                if let serenity::model::application::Interaction::Command(command) = interaction.as_ref() {
+                    match command.data.name.as_str() {
+                        "create_temp" => {
+                            if let Some(guild_id) = command.guild_id {
+                                if let Some(guild_mgr) = &ctx.guild {
+                                    match guild_mgr.create_voice_channel(guild_id.get(), "Temp Channel", None).await {
+                                        Ok(channel_id) => {
+                                            // Move user to new channel if they are in one
+                                            // Note: We need to know if user is in a channel. 
+                                            // Without full cache, we assume user might be in the channel they triggered this from?
+                                            // Or we just create it.
+                                            
+                                            let response = serenity::builder::CreateInteractionResponseMessage::new()
+                                                .content(format!("Created temporary channel: <#{}>", channel_id));
+                                            let builder = serenity::builder::CreateInteractionResponse::Message(response);
+                                            if let Some(responder) = &ctx.responder {
+                                                let _ = responder.respond(command.id.get(), &command.token, builder).await;
+                                            }
+                                        }
+                                        Err(e) => {
+                                            let response = serenity::builder::CreateInteractionResponseMessage::new()
+                                                .content(format!("Failed to create channel: {:?}", e));
+                                            let builder = serenity::builder::CreateInteractionResponse::Message(response);
+                                            if let Some(responder) = &ctx.responder {
+                                                let _ = responder.respond(command.id.get(), &command.token, builder).await;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::VoiceStateUpdate(state) => {
+                // Logic for temp channels:
+                // If user joins "Master" channel -> Create Temp -> Move User
+                // If Temp channel empty -> Delete
+                
+                // For now just log
+                 if let Some(channel_id) = state.channel_id {
+                    info!("User {} joined channel {}", state.user_id, channel_id);
+                 }
+            }
             Event::System(e) => {
                 info!("VoiceTempPlugin received system event: {:?}", e);
             }
